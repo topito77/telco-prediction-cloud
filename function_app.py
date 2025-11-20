82de3738ddfed49c98e2c4df9a7ca1249c58d4f3
Thomas Freundt <93413993+topito77@users.noreply.github.com>
2025-11-20 09:59:13 +0000

    ci: changed function

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+from prediction import make_prediction
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -7,19 +8,21 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def http_trigger_thomas(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    tenure = req.params.get('tenure')
+    monthly = req.params.get('monthly')
+    techsupport = req.params.get('techsupport')
+    
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    prediction = make_prediction(
+        tenure=tenure, 
+        MonthlyCharges=monthly,
+        TechSupport_yes=techsupport
+    )
+
+    if tenure and monthly and techsupport:
+        return func.HttpResponse(f"Hello. The probability that this customer will churn is {prediction}")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+             "This HTTP triggered function executed successfully. Pass tenure, monthly techsupport to get a prediction.",
+             status_code=400
+        )
