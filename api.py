1a77022dc5fba4ca11ef8dcdd717580d9f79eb65
Thomas Freundt <93413993+topito77@users.noreply.github.com>
2025-11-20 10:30:43 +0000

    added notes to function

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 """API for making churn predictions using a FastAPI server.
 
 Add fastapi and uvicorn to your environment with:
-    uv add fastapi uvicorn
+    uv add fastapi uvicorn # it is hosting the api for us
 
 To serve the API, run:
     uv run uvicorn api:app --reload
@@ -21,17 +21,17 @@ class Customer(BaseModel):
     TechSupport_yes: int = Field(alias="techsupport")
 
 
-app = FastAPI()
+app = FastAPI() # makes it easy to define actions
 
 
 @app.post("/predict")
-def predict(customer: Customer):
+def predict(customer: Customer): # taken from customer function
     """Make a churn prediction for a customer."""
-    prediction = make_prediction(**customer.model_dump())
-    return {"prediction": prediction}
+    prediction = make_prediction(**customer.model_dump()) # e.g. tenure=tenure, etc.
+    return {"prediction": prediction} # automatically converted into dictionary through json
 
 
 @app.get("/schema")
 def predict_schema():
     """Describe the expected fields for /predict."""
-    return Customer.model_json_schema()
+    return Customer.model_json_schema() # you can get the schema to run the api
