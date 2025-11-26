9069bfba97a21a4e8270d4770dc5aa27ec7e97de
Thomas Freundt <93413993+topito77@users.noreply.github.com>
2025-11-26 15:05:00 +0000

    updated some changes to our streamlit dashboard

--- docs/dashboard.py
+++ docs/dashboard.py
@@ -1,17 +1,18 @@
+# we are using the following libraries
 import requests
 import streamlit as st
 
 DEV_API = "http://127.0.0.1:8000/predict"
 PROD_API = "https://predictingforjay.azurewebsit..."
 
-
+# fetches status of the response and returns something...
 def fetch_prediction(payload: dict) -> dict:
     """Call the prediction API and return the JSON response"""
     response = requests.post(DEV_API, json=payload, timeout=5)
     response.raise_for_status()
-    return response.json()
+    return response.json() 
 
-
+# second helper function... not sure what either are doing
 def fetch_prediction_from_production(params: dict) -> dict:
     """Call the production API and return the response.
 
@@ -24,6 +25,7 @@ def fetch_prediction_from_production(params: dict) -> dict:
     return response
 
 ### Streamlit Dashboard for Telco Churn Prediction
+# when using streamlit you are usually using st.whatever to call streamlit functions
 
 st.set_page_config(page_title="Telco Churn Predictor", page_icon="📊")
 
@@ -31,10 +33,15 @@ st.set_page_config(page_title="Telco Churn Predictor", page_icon="📊")
 st.title("Telco Churn Prediction")
 st.write("Enter customer details and get a churn prediction.")
 
-tenure = st.selectbox("Tenure (mths)", options=list(range(0, 121)))
+# create two columns, first one on the left, second on the right
+left, right = st.columns(2)
 
-monthly = st.number_input(
-    "Monthly Charges", min_value=0, max_value=1000, step=10, value=70
+# you can get user input using streamlit widgets like select box
+tenure = left.selectbox("Tenure (mths)", options=list(range(0, 121)))
+
+# these are the values the user can select from
+monthly = right.slider(
+    "Monthly Charges", min_value=0, max_value=200, step=10, value=70
 )
 
 techsupport = int(st.toggle("Tech Support", value=False))
